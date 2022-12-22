38e484cd41b4e0c8026264f68178f2a4dc485110
yangongbin <1196141011@qq.com>
2022-12-22 17:51:49 +0800

    路由配置

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-
+import App from '@/App';
+import 'normalize.css'
+import './assets/style/index.less'
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ // <React.StrictMode>
+ <Suspense fallback="loading">
+
+   <HashRouter>
+     <App/>
+   </HashRouter>
+
+</Suspense>
+// </React.StrictMode>
 );
 
 
---
+++ src/views/notFount/index.jsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+const NotFount = memo(() => {
+  return (
+    <div>NotFount</div>
+  )
+})
+
+export default NotFount
